3f821d8fbb21af72f05d3343a1cf59736f1d93f0
Micoder-dev <micoder.com@gmail.com>
2022-07-15 19:53:34 +0530

    instance variables initialized

--- app/src/main/java/com/example/fingerspeedgame/MainActivity.java
+++ app/src/main/java/com/example/fingerspeedgame/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.fingerspeedgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView timerTextView, aThousandTextView;
+    private Button tapTapButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timerTextView = findViewById(R.id.txtTimer);
+        aThousandTextView = findViewById(R.id.txtAThousand);
+        tapTapButton = findViewById(R.id.btnTap);
+
+        tapTapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
     }
 }
